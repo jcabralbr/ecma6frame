ed7f645c65f9161ebaaf231994f6694b2c6b7801
Julio Cesar Cabral <julio.cabral@b2wdigital.com>
2016-07-26 14:20:08 -0300

    criando controller de Negociacao

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,8 +1,25 @@
 class NegociacaoController {
 
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $ ('#valor');
+
+    }
+
     adiciona(event){
         event.preventDefault();
-        alert('chamei acao no controller');
 
+        console.log(typeof(this._inputData.value));
+        console.log(this._inputData.value);
+
+        let negociacao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
     }
 }
