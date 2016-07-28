18a7b470555f04d71e1c54d1b28d6a9bd57b6d05
Julio Cesar Cabral <julio.cabral@b2wdigital.com>
2016-07-28 11:28:23 -0300

    criando DateHelper

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,21 +5,18 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $ ('#valor');
-
     }
 
     adiciona(event){
         event.preventDefault();
-
-        console.log(typeof(this._inputData.value));
-        console.log(this._inputData.value);
-
+        let helper = new DateHelper();
         let negociacao = new Negociacao(
-            new Date(this._inputData.value.replace(/-/g, ',')),
+            helper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
         console.log(negociacao);
+        console.log(helper.dataParaTexto(negociacao.data));
     }
 }
