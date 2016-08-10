dae9d87d0acd806dc4f0f28e2325aa30c7cb9b6e
Julio Cesar Cabral <julio.cabral@b2wdigital.com>
2016-08-10 09:44:53 -0300

    gerando a tabela em NegociacoesView

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -4,7 +4,7 @@ class NegociacoesView {
         this._elemento = elemento;
     }
 
-    _template(){
+    _template(model){
         return `
                  <table class="table table-hover table-bordered">
                      <thead>
@@ -18,14 +18,33 @@ class NegociacoesView {
 
                      <tbody>
                      </tbody>
+                        ${model.negociacoes.map(n =>`
 
+                            <tr>
+                                <td>${DateHelper.dataParaTexto(n.data)}</td>
+                                <td>${n.quantidade}</td>
+                                <td>${n.valor}</td>
+                                <td>${n.volume}</td>
+                            </tr>
+
+                        `).join('')}
                      <tfoot>
+                        <td colspan="3"></td>
+                        <td>${
+                              (function(){
+                                let total = 0;
+                                model.negociacoes.forEach(n => total += n.volume);
+                                return total;
+                             })() //IIFE
+
+                           }
+                        </td>
                      </tfoot>
                  </table>`
     }
 
-    update(){
-        this._elemento.innerHTML = this._template();
+    update(model){
+        this._elemento.innerHTML = this._template(model);
     }
 }
 
